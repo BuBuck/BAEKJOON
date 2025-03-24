e12556012ec902646e6901c3d107ac301b3a2c03
BuBuck <sunjeongin123@gmail.com>
2025-03-24 16:40:15 +0900

    .

---
+++ 단계별로 풀기/C++/3단계 반복문/12_10951.cpp
@@ -0,0 +1,14 @@
+// A+B - 4
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+
+}
